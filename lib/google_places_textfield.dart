d0e09c0c4a40d1c45b85ad4a6d48604f33d0b030
syedabdulbasi1 <basit@get-licensed.co.uk>
2024-07-19 19:48:58 +0500

    Update google_places_textfield.dart

--- lib/google_places_textfield.dart
+++ lib/google_places_textfield.dart
@@ -157,7 +157,7 @@ class _GooglePlaceAutoCompleteTextFieldState
 
       /// Add the custom header to the options
       final options = Options(headers: widget.headers);
-      Response response = await _dio.get(url, options);
+      Response response = await _dio.get(url, options: options);
       ScaffoldMessenger.of(context).hideCurrentSnackBar();
 
       Map map = response.data;
